43b742227fbe0fe70eca74438caf13a25b6719d3
Lukas Kohlmaier <luko0610@gmail.com>
2023-11-11 23:00:47 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+</script>
+
+<h1>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</h1>
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  // Import your profile picture
+</script>
+
+<div class="grid grid-cols-3 gap-8" style="height: calc(100vh - 128px);">
+  <div class="col-span-1 flex justify-center align-middle">
+    <img
+      class="w-full"
+      src="https://placehold.co/200x250"
+      alt="Profile"
+    />
+  </div>
+  <div class="col-span-2">
+    <div class="flex items-center h-full">
+      <div>
+        <h1 class="text-3xl font-bold mb-4">About Me</h1>
+        <p class="text-lg mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          euismod libero, vel bibendum sapien. Nulla facilisi. Sed euismod
+          euismod libero, vel bibendum sapien. Nulla facilisi.
+        </p>
+        <p class="text-lg mb-4">
+          Sed euismod euismod libero, vel bibendum sapien. Nulla facilisi. Sed
+          euismod euismod libero, vel bibendum sapien. Nulla facilisi.
+        </p>
+        <p class="text-lg mb-4">Sed euismod euismod libero,</p>
+      </div>
+    </div>
+  </div>
+</div>
